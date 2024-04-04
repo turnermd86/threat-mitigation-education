688b53a19155fafa52ab6ff90d444211c6b88471
Matthew Turner <mturner_19_06@hotmail.com>
2024-04-04 16:14:03 -0500

    modified:   conf.py

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md', 'requiremen
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'furo'
 
 html_static_path = ['_static']
